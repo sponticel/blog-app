afb55150082c522a7f269f7390066c351c57abfe
jarrett <jarrettpdunne@gmail.com>
2021-03-29 21:53:09 -0500

    update blog.js to correct naming convention

--- models/blog.js
+++ models/blog.js
@@ -3,14 +3,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Blog = new Schema(
+const Post = new Schema(
   {
-    title: { type: String, required: true },
+    name: { type: String, required: true },
     imgURL: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: String, required: true }
+    description: { type: String, required: true },
+    price: { type: String, required: true }
   },
   { timestamps: true }
 )
 
-module.exports = mongoose.model('blogs', Blog)
+module.exports = mongoose.model('posts', Post)
